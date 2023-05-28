69a7d583fda8c47743c0c9b88258857ff075d5b4
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-05-28 23:34:19 +0530

    Configured the current user in the layout and passed it to the navbar

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { User } from "@prisma/client";
+
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
 
-const Navbar = () => {
+interface NavBarProps {
+    currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavBarProps> = ({ currentUser }) => {
+    console.log(currentUser);
+
     return (
         <div className="fixed w-full bg-wight z-10 shadow-sm">
             <div className="py-4 border-b-[1px]">
