0f1def41e1867f1fbcf9ed15c1d4f4839fe6301f
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-05-30 13:30:23 +0530

    Added the category heading and functionalities to retrieve all categories

--- src/app/components/modals/RentModal.tsx
+++ src/app/components/modals/RentModal.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import useRentModal from "@/app/hooks/useRentModal";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 import Modal from "./Modal";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
 
 enum STEPS {
     CATEGORY = 0,
@@ -20,14 +22,65 @@ const RentModal = () => {
 
     const [step, setStep] = useState(STEPS.CATEGORY);
 
+    const onBack = () => {
+        setStep((value) => value - 1);
+    }
+
+    const onNext = () => {
+        setStep((value) => value + 1);
+    }
+
+    const actionLabel = useMemo(() => {
+        if (step === STEPS.PRICE) {
+            return "Create";
+        } else {
+            return "Next";
+        }
+    }, [step]);
+
+    const secondaryActionLabel = useMemo(() => {
+        if (step === STEPS.CATEGORY) {
+            return undefined;
+        } else {
+            return "Back";
+        }
+    }, [step]);
+
+    let bodyContent = (
+        <div className="flex flex-col gap-8">
+            <Heading
+                title="Which of these best describes your place?"
+                subtitle="Pick a Category"
+            />
+            <div 
+                className="
+                    grid
+                    grid-cols-1
+                    md:grid-cols-2
+                    gap-3
+                    max-h-[50vh]
+                    overflow-y-auto
+                ">
+                {categories.map((item) => (
+                    <div key={item.label} className="col-span-1">
+                        {item.label}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+    
+
     return (
         <Modal 
             title="Airbnb your home" 
             isOpen={rentModal.isOpen}
             onClose={rentModal.onClose}
             onSubmit={rentModal.onClose}
-            actionLabel="Submit"
-
+            actionLabel={actionLabel}
+            secondaryActionLabel={secondaryActionLabel}
+            secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
+            body={bodyContent}
          />
     );
 }
