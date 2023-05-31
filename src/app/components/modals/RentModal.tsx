608399d823a6916d76deb792bb795216eb7cf2a1
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-05-31 23:59:31 +0530

    Completed app compoenns

--- src/app/components/modals/RentModal.tsx
+++ src/app/components/modals/RentModal.tsx
@@ -7,9 +7,9 @@ import Modal from "./Modal";
 import Heading from "../Heading";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
-import Map from "../Map";
 import { categories } from "../navbar/Categories";
 import { FieldValues, useForm } from "react-hook-form";
+import dynamic from "next/dynamic";
 
 enum STEPS {
     CATEGORY = 0,
@@ -51,6 +51,11 @@ const RentModal = () => {
 
     const category = watch("category");
     const location = watch("location");
+
+    //dynamically re-rendering Map component
+    const Map = useMemo(() => dynamic(() => import("../Map"), {
+        ssr: false
+    }), [location]);
 
     const setCustomValue = (id: string, value: any) => {
         setValue(id,value, {
@@ -125,7 +130,7 @@ const RentModal = () => {
                     value={location}
                     onChange={(value) => setCustomValue("location", value)}
                 />
-                <Map />
+                <Map center={location?.latlng} />
             </div>
         )
     }
