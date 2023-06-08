df364ddd4968759a67e4dcb200616b89e2fee135
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-06-08 19:29:09 +0530

    Completed implementing the listing card

--- src/app/components/listings/ListingCard.tsx
+++ src/app/components/listings/ListingCard.tsx
@@ -1,11 +1,13 @@
 "use client";
-
-import useCountries from "@/app/hooks/useCountries";
-import { SafeUser } from "@/app/types";
-import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import { useCallback, useMemo } from "react";
 import format from "date-fns/format";
+import Image from "next/image";
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+import { Listing, Reservation } from "@prisma/client";
+import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
     data: Listing;
@@ -64,8 +66,64 @@ const ListingCard: React.FC<ListingCardProps> = ({
     }, [reservation]);
 
     return (
-        <div>
-            Enter
+        <div
+            onClick={() => router.push(`/listings/${data.id}`)}
+            className="
+                col-span-1 cursor-pointer group
+            "
+        >
+            <div className="flex flex-col gap-2 w-full">
+                <div
+                    className="
+                        aspect-square
+                        w-full
+                        relative
+                        overflow-hidden
+                        rounded-xl
+                    "
+                >
+                    <Image 
+                        fill
+                        alt="Listing"
+                        src={data.imageSrc}
+                        className="
+                            object-cover
+                            h-full
+                            w-full
+                            group-hover:scale-110
+                            transition
+                        "
+                    />
+                    <div className="absolute top-3 right-3">
+                        <HeartButton 
+                            listingId={data.id}
+                            currentUser={currentUser}
+                        />
+                    </div>
+                </div>
+                <div className="font-semibold text-lg">
+                    {location?.region}, {location?.label}
+                </div>
+                <div className="font-light text-neutral-500">
+                    {reservationDate || data.category}
+                </div>
+                <div className="flex flex-row items-center gap-1">
+                    <div className="font-semibold">
+                        $ {price}
+                    </div>
+                    {!reservation && (
+                        <div className="font-light">night</div>
+                    )}
+                </div>
+                {onAction && actionLabel && (
+                    <Button 
+                        disabled={disabled}
+                        small
+                        label={actionLabel}
+                        onClick={handleCancel}
+                    />
+                )}
+            </div>
         </div>
     );
 }
