6b21b99fd25e41d351a012cf0aac91a451ef4859
anusaraweerasooriya <anusaraweerasooriya@gmail.com>
2023-06-20 01:37:52 +0530

    4rth deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
-import getCurrentUser from "./actions/getCurrentUser";
-import getListings, { IListingsParams } from "./actions/getListings";
-import ClientOnly from "./components/ClientOnly";
+
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 import ListingCard from "./components/listings/ListingCard";
 
+import getListings, { 
+  IListingsParams
+} from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import ClientOnly from "./components/ClientOnly";
+
 interface HomeProps {
-  searchParams: IListingsParams;
-}
+  searchParams: IListingsParams
+};
 
-
-const Home = async ({ searchParams } : HomeProps) => {
-  
+const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
-  
+
   if (listings.length === 0) {
     return (
       <ClientOnly>
@@ -23,32 +25,29 @@ const Home = async ({ searchParams } : HomeProps) => {
     );
   }
 
-  
   return (
     <ClientOnly>
       <Container>
         <div 
           className="
             pt-24
-            grid
-            grid-cols-1
-            sm:grid-cols-2
-            md:grid-cols-3
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
             lg:grid-cols-4
             xl:grid-cols-5
             2xl:grid-cols-6
             gap-8
           "
         >
-          {listings.map((listing) => {
-            return (
-              <ListingCard 
-                currentUser={currentUser}
-                key={listing.id}
-                data={listing}
-              />
-            )
-            })}
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
